8b31ac3853b48f2fe8818f3d3da9338bad24e9c1
Ayan564 <ayanmaity564@gmail.com>
2024-03-15 19:19:45 +0530

    Add Navigation and start working on it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; // Import createRoot from react-dom/client
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
 );
+// Use createRoot from react-dom/client
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
