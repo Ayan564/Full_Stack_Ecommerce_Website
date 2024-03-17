971d9b536dcb9e794e7fb07c6e32ad4738bd1fd4
Ayan564 <ayanmaity564@gmail.com>
2024-03-17 22:09:54 +0530

    Add frontend login and register

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,25 @@ import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+//auth
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Resister.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Route>
+  )
 );
 // Use createRoot from react-dom/client
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
