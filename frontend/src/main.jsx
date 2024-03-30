6ac0b1ff40fbe2b960b2bde6b754d3225835523e
Ayan564 <ayanmaity564@gmail.com>
2024-03-30 22:20:35 +0530

    Create some frontend function

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,12 +10,23 @@ import store from "./redux/store.js";
 //auth
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Resister.jsx";
+import Profile from "./pages/User/Profile.jsx";
+
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+      </Route>
     </Route>
   )
 );
