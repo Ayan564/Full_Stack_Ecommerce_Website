1ac7b6ad0dcecafa5d8388ace00f70418242f339
Ayan564 <ayanmaity564@gmail.com>
2024-03-31 11:07:20 +0530

    Create the frontend of Category Section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,7 @@ import Profile from "./pages/User/Profile.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import AdminRoute from "./pages/Admin/AdminRoute.jsx";
 import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       </Route>
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
       </Route>
     </Route>
   )
