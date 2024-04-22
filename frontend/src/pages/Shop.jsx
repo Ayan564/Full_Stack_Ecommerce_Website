8d0d32cc16227d279703ca5ec1e15d6299089757
Ayan564 <ayanmaity564@gmail.com>
2024-04-22 20:16:44 +0530

    Change the style of the checkbox

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -127,11 +127,11 @@ const Shop = () => {
               {uniqueBrands?.map((brand) => (
                 <div key={brand} className="flex items-enter mr-4 mb-5">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id={brand}
                     name="brand"
                     onChange={() => handleBrandClick(brand)}
-                    className="w-4 h-4 accent-pink-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                    className="w-4 h-4 accent-pink-600 bg-gray-100  border-gray-400 rounded-lg focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
                   <label
                     htmlFor="pink-radio"
